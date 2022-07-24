b7dd58dd121c5fe8b3f18fb390fccb465291286c
Mizuno66 <kouta126@hotmail.co.jp>
2022-07-24 20:39:27 +0900

    スワイプで更新する処理を追加

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -2,16 +2,19 @@ import 'package:ec_test/providers/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:ec_test/models/products.dart';
 import 'package:ec_test/repository/products_repository.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context){
+    final products = useState(ProductRepository().fetch());
+
     return FutureBuilder
     (
-      future: ProductRepository().fetch(),
+      future: products.value,
       builder: (context, AsyncSnapshot<List<Product>> snapshot) {
         if (snapshot.connectionState != ConnectionState.done)
         {
@@ -28,50 +31,55 @@ class MyHomePage extends StatelessWidget {
           return const Text("データなし");
         }
 
-        return (GridView.count
-        (
-          crossAxisCount: 3,
-          children: List.generate(snapshot.data!.length, (int i) => Container
-          (
-            alignment: Alignment.center,
-            child: Column
+        return RefreshIndicator(
+          onRefresh: () async {
+            products.value = ProductRepository().fetch();
+          },
+          child: (GridView.count
             (
-              children: <Widget> [
-                Text(snapshot.data![i].getName()),
-                Consumer(builder: (context, ref, _) {
-                  final cart = ref.watch(cartProvider);
-                  return(
-                    Row
-                    (
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 24,
-                          height: 24,
-                          child: FloatingActionButton(
-                            onPressed: () => ref.read(cartProvider.notifier).remove(snapshot.data![i].id),
-                            backgroundColor: Colors.blue,
-                            child: const Icon(Icons.remove),
-                          ),
-                        ),
-                        Text((cart[snapshot.data![i].id] ?? 0).toString()),
-                        SizedBox(
-                          width: 24,
-                          height: 24,
-                          child: FloatingActionButton(
-                            onPressed: () => ref.read(cartProvider.notifier).add(snapshot.data![i].id),
-                            backgroundColor: Colors.blue,
-                            child: const Icon(Icons.add),
-                          ),
-                        ),
-                      ]
-                    )
-                  );
-                })
-              ],
-            )
-          ))
-        ));
+              crossAxisCount: 3,
+              children: List.generate(snapshot.data!.length, (int i) => Container
+              (
+                alignment: Alignment.center,
+                child: Column
+                (
+                  children: <Widget> [
+                    Text(snapshot.data![i].getName()),
+                    Consumer(builder: (context, ref, _) {
+                      final cart = ref.watch(cartProvider);
+                      return(
+                        Row
+                        (
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: FloatingActionButton(
+                                onPressed: () => ref.read(cartProvider.notifier).remove(snapshot.data![i].id),
+                                backgroundColor: Colors.blue,
+                                child: const Icon(Icons.remove),
+                              ),
+                            ),
+                            Text((cart[snapshot.data![i].id] ?? 0).toString()),
+                            SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: FloatingActionButton(
+                                onPressed: () => ref.read(cartProvider.notifier).add(snapshot.data![i].id),
+                                backgroundColor: Colors.blue,
+                                child: const Icon(Icons.add),
+                              ),
+                            ),
+                          ]
+                        )
+                      );
+                    })
+                  ],
+                )
+              ))
+            ))
+        );
       }
     );
   }
